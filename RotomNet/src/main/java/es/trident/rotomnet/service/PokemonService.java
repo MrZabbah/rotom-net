c98bf94793b13ec89347fec6fb918338ff51b921
mrzabbah <56138357+MrZabbah@users.noreply.github.com>
2022-02-12 16:12:34 +0100

    Cards template added
    Se ha añadido la plantilla y visualización de las cartas
    
    Se trata de una primera aproximación al sistema de cartas de la aplicación, teniendo en cuenta las propiedades que estas puedan presentar. Estas son tratadas también con CSS.

--- RotomNet/src/main/java/es/trident/rotomnet/service/PokemonService.java
+++ RotomNet/src/main/java/es/trident/rotomnet/service/PokemonService.java
@@ -14,32 +14,37 @@ public class PokemonService {
 				List.of(				
 						new PokemonCard(0, false, false),
 						new PokemonCard(1, false, false),
-						new PokemonCard(2, false, false)
+						new PokemonCard(2, false, false),
+						new PokemonCard(3, true, true),
+						new PokemonCard(4, false, false)
 						),
 				List.of(				
-						new PokemonCard(3, true, false),
-						new PokemonCard(4, false, false),
-						new PokemonCard(5, false, false)
-						),
-				List.of(				
-						new PokemonCard(6, false, false),
+						new PokemonCard(5, false, false),
+						new PokemonCard(6, true, true),
 						new PokemonCard(7, false, false),
-						new PokemonCard(8, false, false)
+						new PokemonCard(8, true, false),
+						new PokemonCard(9, false, false)
 						),
 				List.of(				
-						new PokemonCard(9, true, true),
 						new PokemonCard(10, false, false),
-						new PokemonCard(11, false, false)
-						),
-				List.of(				
-						new PokemonCard(12, true, true),
-						new PokemonCard(13, false, false),
+						new PokemonCard(11, false, false),
+						new PokemonCard(12, false, false),
+						new PokemonCard(13, true, false),
 						new PokemonCard(14, false, false)
 						),
 				List.of(				
 						new PokemonCard(15, false, false),
 						new PokemonCard(16, true, false),
-						new PokemonCard(17, false, false)
+						new PokemonCard(17, false, false),
+						new PokemonCard(18, true, false),
+						new PokemonCard(19, true, true)
+						),
+				List.of(				
+						new PokemonCard(20, true, true),
+						new PokemonCard(21, true, true),
+						new PokemonCard(22, false, false),
+						new PokemonCard(23, true, false),
+						new PokemonCard(24, false, false)
 						)
 				);
 	}
