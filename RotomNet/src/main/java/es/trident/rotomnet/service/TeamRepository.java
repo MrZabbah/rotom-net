11cc8fc64af6c1119be092a0b9f25067d5d3cabd
David González <60218308+DavidGRueda@users.noreply.github.com>
2022-02-15 18:24:37 +0100

    Deshacer Merge de Main en User.

--- RotomNet/src/main/java/es/trident/rotomnet/service/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package es.trident.rotomnet.service;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.trident.rotomnet.model.Team;
-
-public interface TeamRepository extends JpaRepository<Team,Integer>{
-
-}
