0829624cf7640589bdf2147b6aa0e4f8d1a176b3
mrzabbah <56138357+MrZabbah@users.noreply.github.com>
2022-02-17 15:04:01 +0100

    ARREGLO PROYECTO: Cambio de main al anterior
    Se elimina el proyecto dañado y se coloca la versión previa al Merge dañino.
    
    La versión previa se encuentra como Backup en la rama main-save-#1, generada antes de realizar el Pull por seguridad

--- RotomNet/src/main/java/es/trident/rotomnet/repository/PokemonRepository.java
+++
@@ -1,9 +0,0 @@
-package es.trident.rotomnet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.trident.rotomnet.model.Pokemon;
-
-public interface PokemonRepository extends JpaRepository<Pokemon, Integer>{
-
-}
--- RotomNet/src/main/java/es/trident/rotomnet/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package es.trident.rotomnet.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.trident.rotomnet.model.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
