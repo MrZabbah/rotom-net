b79358f0d8d44e5292911afa24bc138598010756
mrzabbah <56138357+MrZabbah@users.noreply.github.com>
2022-03-08 14:15:31 +0100

    Plantilla base funcional con seguridad
    Proyecto con sistema básico de seguridad, un login y un logout.
    
    La privacidad de las páginas de este commit es puramente de ejemplo, para comprobar que el sistema de seguridad funciona correctamente. De esta forma, se pueden generar bifurcaciones de esta plantilla para generar la implementación real

--- RotomNet/src/main/java/es/trident/rotomnet/security/SecurityConfiguration.java
+++ RotomNet/src/main/java/es/trident/rotomnet/security/SecurityConfiguration.java
@@ -1,16 +1,26 @@
 package es.trident.rotomnet.security;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		
+		String encodedPass = encoder.encode("pkmn");
+		
 		// Administrator adding
-		auth.inMemoryAuthentication().withUser("opalo").password("pkmn").roles("USER");
+		auth.inMemoryAuthentication().withUser("opalo").password(encodedPass).roles("USER");
+		
+		
 	}
 
 	@Override
@@ -21,6 +31,16 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		// CSRF disabling
 		http.authorizeHttpRequests().antMatchers("/pokedex").authenticated();
 		http.authorizeHttpRequests().anyRequest().permitAll();
+		
+		http.formLogin().loginPage("/login");
+		http.formLogin().usernameParameter("username");
+		http.formLogin().passwordParameter("pwd");
+
+		http.formLogin().defaultSuccessUrl("/");
+		http.formLogin().failureUrl("/");
+		
+		http.logout().logoutUrl("/logout");
+		http.logout().logoutSuccessUrl("/");
 		
 		http.csrf().disable();
 	}
