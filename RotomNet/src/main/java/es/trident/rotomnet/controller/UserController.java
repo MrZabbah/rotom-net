2166934567c7fa67aff9ff49a9c51f122a0b22ba
David González <60218308+DavidGRueda@users.noreply.github.com>
2022-03-10 17:27:53 +0100

    Implementación de logueo de usuarios
    Funcionalidad completa de Login y Logout de usuarios.
    
    Se incluyen:
    - Definición de páginas públicas y privadas en SecurityConfiguration.
    - Creación Front-end de la página de login de usuarios.
    - Modificación de página de todos los usuarios para que solo tenga acceso el administrador y disponga de toda la información de los mismos.
    - Creación de un método @ ModelAtrtribute para mostrar los enlaces correspondientes en la cabecera dependiendo de si se está o no logueado.
    - Modificación de algunos métodos del controlador  y páginas de creación y display de equipos para adaptación a nuevo sistema de direccionamiento y al login.
    - Modificaciones menores de battle.html y otros para adaptarse a las nuevas necesidades de la aplicación.

--- RotomNet/src/main/java/es/trident/rotomnet/controller/UserController.java
+++ RotomNet/src/main/java/es/trident/rotomnet/controller/UserController.java
@@ -98,7 +98,7 @@ public class UserController {
 		return "redirect:/users";
 	}
 	
-	@PostMapping("/modify/{username}")
+	@GetMapping("/modify/{username}")
 	public String modifySelectedUser(Model model, @PathVariable String username) {
 		User user = userService.findUserByUsername(username);
 		
@@ -107,11 +107,6 @@ public class UserController {
 		return "modify";
 	}
 	
-	@GetMapping("/selectUser")
-	public String userTable(Model model) {		
-		model.addAttribute("users", userService.getAllUsers());		
-		return "aux_users";
-	}
 	
 	/**
 	 * Descarga la imagen de usuario si la tiene. En caso contrario, devuelve una imagen por 
--- RotomNet/src/main/java/es/trident/rotomnet/security/SecurityConfiguration.java
+++ RotomNet/src/main/java/es/trident/rotomnet/security/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package es.trident.rotomnet.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,8 +18,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		
 		String encodedPass = encoder.encode("pkmn");
 		
-		// Administrator adding
-		auth.inMemoryAuthentication().withUser("opalo").password(encodedPass).roles("USER");
+		// Administrator addings
+		auth.inMemoryAuthentication().withUser("opalo").password(encodedPass).roles("USER", "ADMIN");
+		auth.inMemoryAuthentication().withUser("Test").password(encodedPass).roles("USER");
 		
 		
 	}
@@ -28,20 +30,39 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 		
 		// Public and Private pages
 		// Login - Logout Configuration
-		// CSRF disabling
-		http.authorizeHttpRequests().antMatchers("/pokedex").authenticated();
-		http.authorizeHttpRequests().anyRequest().permitAll();
+		// CSRF disabling		
+		http.authorizeHttpRequests().antMatchers("/").permitAll();
+		http.authorizeHttpRequests().antMatchers("/login").permitAll();
+		http.authorizeHttpRequests().antMatchers("/register").permitAll();
+		http.authorizeHttpRequests().antMatchers("/registered").permitAll();
+		http.authorizeHttpRequests().antMatchers("/pokedex").permitAll();
+		http.authorizeHttpRequests().antMatchers("/teamGenerator").permitAll();
+		http.authorizeHttpRequests().antMatchers("/createRandomTeam").permitAll();
+		http.authorizeHttpRequests().antMatchers("/createRandomTeam").permitAll();
+		http.authorizeHttpRequests().antMatchers("/{id}/image").permitAll();		
+		http.authorizeHttpRequests().antMatchers("/users").hasRole("ADMIN");
+		http.authorizeHttpRequests().antMatchers("/selectUser").hasRole("ADMIN");
+		
+		http.authorizeHttpRequests().anyRequest().authenticated();
+		
 		
 		http.formLogin().loginPage("/login");
 		http.formLogin().usernameParameter("username");
 		http.formLogin().passwordParameter("pwd");
-
-		http.formLogin().defaultSuccessUrl("/");
 		http.formLogin().failureUrl("/");
+		http.formLogin().defaultSuccessUrl("/", true);
+		
 		
 		http.logout().logoutUrl("/logout");
 		http.logout().logoutSuccessUrl("/");
 		
 		http.csrf().disable();
 	}
+	
+	@Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
 }
